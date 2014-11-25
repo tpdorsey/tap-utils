8d6fe8ad4d407a33fd3694d29318a6acd3eca756
Terrence Dorsey <terrend@mishu.com>
2014-11-24 23:29:31 -0500

    basic functionality

--- tap-timeline.rb
+++ tap-timeline.rb
@@ -3,39 +3,44 @@
 # tap-timeline.rb - returns a timeline of grades for a given style
 
 require 'json'
+require 'date'
 
 # Lookup table for float grade to letter grade
 # Involves some guesswork but I think it's pretty close
 def gradeLookup(grade)
   if grade <= 0
-    return "Not graded"
+    return ["Not graded", 0]
   elsif grade >= 3.8
-    return "A+"
+    return ["A+", 13]
   elsif grade >= 3.47
-    return "A"
+    return ["A",  12]
   elsif grade >= 3.14
-    return "A-"
+    return ["A-", 11]
   elsif grade >= 2.81
-    return "B+"
+    return ["B+", 10]
   elsif grade >= 2.48
-    return "B"
+    return ["B",  9]
   elsif grade >= 2.15
-    return "B-"
+    return ["B-", 8]
   elsif grade >= 1.82
-    return "C+"
+    return ["C+", 7]
   elsif grade >= 1.49
-    return "C"
+    return ["C",  6]
   elsif grade >= 1.18
-    return "C-"
+    return ["C-", 5]
   elsif grade >= 0.83
-    return "D+"
+    return ["D+", 4]
   elsif grade >= 0.50
-    return "D"
+    return ["D",  3]
   elsif grade >= 0.17
-    return "D-"
+    return ["D-", 2]
   else
-    return "F"
+    return ["F",  1]
   end
+end
+
+def parse_timestamp(date)
+  return DateTime.strptime(date, "%Y-%m-%d %H:%M:%S %Z")
 end
 
 # Assume first argument is style, second is JSON
@@ -58,17 +63,27 @@ file_hash["tapcellarbeers"].each do |beer_record|
     if beer_record["bdb_style"].to_s.downcase.include? style_keyword or beer_record["style"].to_s.downcase.include? style_keyword
 
       if beer_record.has_key?("tastings")
-        grade_date = beer_record["tastings"][0]["timestamp"]
+        timestamp = parse_timestamp(beer_record["tastings"][0]["timestamp"])
       else
-        grade_date = beer_record["editdate"]
+        timestamp = parse_timestamp(beer_record["editdate"])
       end
 
       grade_data = gradeLookup(grade)
 
-      beerGrades << [grade_date, grade_data]
+      beerGrades << [timestamp, grade_data[0], grade_data[1]]
     end
 
   end
 end
 
-puts beerGrades
+beerGrades.sort! { |a, b| a[0] <=> b[0] }
+
+puts "Grade timeline for styles containing: " + style_keyword.split.map(&:capitalize).join(' ')
+puts ""
+puts "Date".ljust(12) + "Grade F" + "A+".rjust(12)
+beerGrades.each do |beer|
+  puts beer[0].strftime("%Y-%m-%d") + "  " + beer[1].ljust(4) + (' ' * beer[2]) + "*"
+end
+
+
+
