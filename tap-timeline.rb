e6d1236b7ad91f1a542cc2c286645785fb43a9a0
Terrence Dorsey <terrend@mishu.com>
2014-12-18 15:33:31 -0500

    added beer name to output

--- tap-timeline.rb
+++ tap-timeline.rb
@@ -4,6 +4,7 @@
 
 require 'json'
 require 'date'
+require 'optparse'
 
 # Lookup table for float grade to letter grade
 # Involves some guesswork but I think it's pretty close
@@ -43,12 +44,34 @@ def parse_timestamp(date)
   return DateTime.strptime(date, "%Y-%m-%d %H:%M:%S %Z")
 end
 
-# Assume first argument is style, second is JSON
-style_keyword = ARGV[0].downcase
-file_to_read = ARGV[1]
+ARGV << '-h' if ARGV.length < 2
 
-# Read and hash JSON
-file_hash = JSON.parse(File.open(file_to_read, 'r').read, :max_nesting => 100)
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Returns a timeline of grades for a given style in a TapCellar backup.\nUsage: tap-timeline.rb [options] [style_keyword] [file to parse]"
+  options[:csv] = false
+  options[:filename] = ""
+  options[:keyword] = ""
+
+  opts.on("-c", "--csv", "Output CSV") do
+    options[:csv] = true
+  end
+
+  opts.on('-h', '--help', 'Display help for options' ) do
+    puts opts
+    exit
+  end
+end.parse!
+
+options[:filename] = ARGV[ARGV.length - 1]
+options[:keyword] = ARGV[ARGV.length - 2].downcase
+
+if File.exists?(options[:filename])
+  file_hash = JSON.parse(File.open(options[:filename], 'r').read, :max_nesting => 100)
+else
+  puts "Error: File " + options[:filename] + " does not exist."
+  exit
+end
 
 # We'll keep data for each grade in this array
 beerGrades = Array.new
@@ -60,7 +83,7 @@ file_hash["tapcellarbeers"].each do |beer_record|
   grade = beer_record["grade"].to_f.round(3)
   if grade > 0
 
-    if beer_record["bdb_style"].to_s.downcase.include? style_keyword or beer_record["style"].to_s.downcase.include? style_keyword
+    if beer_record["bdb_style"].to_s.downcase.include? options[:keyword] or beer_record["style"].to_s.downcase.include? options[:keyword]
 
       if beer_record.has_key?("tastings")
         timestamp = parse_timestamp(beer_record["tastings"][0]["timestamp"])
@@ -70,7 +93,7 @@ file_hash["tapcellarbeers"].each do |beer_record|
 
       grade_data = gradeLookup(grade)
 
-      beerGrades << [timestamp, grade_data[0], grade_data[1]]
+      beerGrades << [timestamp, grade_data[0], grade_data[1], beer_record["beername"]]
     end
 
   end
@@ -78,11 +101,11 @@ end
 
 beerGrades.sort! { |a, b| a[0] <=> b[0] }
 
-puts "Grade timeline for styles containing: " + style_keyword.split.map(&:capitalize).join(' ')
+puts "Grade timeline for styles containing: " + options[:keyword].split.map(&:capitalize).join(' ')
 puts ""
-puts "Date".ljust(12) + "Grade F" + "A+".rjust(12)
+puts "Date".ljust(12) + "Grade F" + "A+".rjust(12) + "  Name"
 beerGrades.each do |beer|
-  puts beer[0].strftime("%Y-%m-%d") + "  " + beer[1].ljust(4) + (' ' * beer[2]) + "*"
+  puts beer[0].strftime("%Y-%m-%d") + "  " + beer[1].ljust(4) + (' ' * beer[2]) + "*" + (' ' * (16 - beer[2])) + beer[3]
 end
 
 
